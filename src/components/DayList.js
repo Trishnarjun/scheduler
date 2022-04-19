c4645decd280aac191e7ee3b2228da4263e62bf0
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-19 18:53:04 -0400

    bulding of the daylist complete and updating Application.js to make it work

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,21 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList(props) {
+export default function DayList(props,days) {
+  console.log(props.days);
+  const dayLists = props.days.map((dayList) => {
+    return (
+      <DayListItem 
+        key={dayList.id}
+        name={dayList.name} 
+        spots={dayList.spots} 
+        selected={dayList.name === props.day}
+        setDay={props.setDay}  
+      />
+    )
+  })
+
   return (
-    <ul>  
-      
-    </ul>
+    <ul>{dayLists}</ul>
   )
 }
