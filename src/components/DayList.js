e9b3c22b4a8c26f0e0e40bd11d32a645b22e322d
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-20 09:09:33 -0400

    building the interviewer item

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,6 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props,days) {
-  console.log(props.days);
   const dayLists = props.days.map((dayList) => {
     return (
       <DayListItem 
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const InterviewerListItemClass = classNames("interviewers__item-image", {
+    "interviewers__item--selected": props.selected
+  })
+
+  
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={InterviewerListItemClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name }
+    </li>
+  )
+}
